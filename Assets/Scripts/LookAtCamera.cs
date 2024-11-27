9c041b9b83a86238765157e45a550bf4116299f7
alkuramshina <alkuramshina@gmail.com>
2024-11-27 12:37:56 +0300

    look at me script

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    [SerializeField] private Camera cameraToLookAt;
+    [SerializeField] private float rotationSpeed = 3f;
+
+    private void Update()
+    {
+        transform.rotation =
+            Quaternion.Slerp(transform.rotation, cameraToLookAt.transform.rotation, rotationSpeed * Time.deltaTime);
+    }
+}
